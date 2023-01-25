06336ed4f13da742cd90068834b3daec83b5ea53
Mantvydas Liobė <mantvydasliobe@gmail.com>
2023-01-25 17:25:52 +0200

    useCallback to stop API calls

--- src/api/apiCall.ts
+++ src/api/apiCall.ts
@@ -1,24 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useCallback } from "react";
 
-import { useApiDataContext } from '../context/ApiDataContext';
-import { API_LINK } from '../constants/links';
+import { useApiDataContext } from "../context/ApiDataContext";
+import { API_LINK } from "../constants/links";
 
 export const useAPICall = () => {
-  //Maybe useMemo or useCallback for performance...
   const { setApiData, setError } = useApiDataContext();
 
-  const dataFromAPI = async () => {
+  const dataFromAPI = useCallback(async () => {
     try {
       const response = await fetch(API_LINK);
       if (!response.ok) {
         setError('Something went wrong!!!!');
       }
       const data = await response.json();
-      setApiData(data);
+      setApiData(data)
     } catch (error) {
       setError(JSON.stringify(error));
     }
-  };
+  }, [setApiData, setError]);
 
   useEffect(() => {
     dataFromAPI();
