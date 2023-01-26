9389a1a4c105578c95921f7664d6c91f120ee773
Mantvydas Liobė <mantvydasliobe@gmail.com>
2023-01-26 09:09:38 +0200

    husky with pre commit added

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import Countries from './components/Countries';
 import { ApiDataContextProvider } from './context/ApiDataContext';
 
 import './App.css';
+import ButtonsSection from './components/ButtonsSection';
 
 function App() {
   return (
     <div className="App">
       <ApiDataContextProvider>
+        <ButtonsSection />
         <Countries />
       </ApiDataContextProvider>
     </div>
