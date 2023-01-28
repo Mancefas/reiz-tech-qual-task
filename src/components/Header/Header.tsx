b2a61a3a573f99a8424dc0b1b140e36008c8034a
Mantvydas Liobė <mantvydasliobe@gmail.com>
2023-01-28 10:12:20 +0200

    added header, some scss changes

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './header.scss';
+
+export const Header = () => {
+  return (
+    <header className="header">
+      <div className="header__logo">
+        <h1>REIZ TECH assignment</h1>
+      </div>
+    </header>
+  );
+};
